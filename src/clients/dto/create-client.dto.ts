810251acf2d148ce201f7c6a8c7ed0c4ab22fd87
Isaias Bermudez <166860546+cheems-chems@users.noreply.github.com>
2026-03-11 18:36:50 -0500

    chore(clients): update controller and dtos

--- src/clients/dto/create-client.dto.ts
+++ src/clients/dto/create-client.dto.ts
@@ -1,6 +1,22 @@
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
+import { IsOptional, IsString, IsUUID } from 'class-validator';
+
 export class CreateClientDto {
+  @ApiProperty()
+  @IsUUID()
   tenantId: string;
+
+  @ApiProperty()
+  @IsString()
   phone: string;
+
+  @ApiPropertyOptional()
+  @IsOptional()
+  @IsString()
   name?: string;
+
+  @ApiPropertyOptional()
+  @IsOptional()
+  @IsString()
   notes?: string;
 }
