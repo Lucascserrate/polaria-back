7d451ac21ec6359586d8c5fadcedfe0078a74b85
Isaias Bermudez <166860546+cheems-chems@users.noreply.github.com>
2026-03-11 18:36:43 -0500

    chore(services): update controller and dtos

--- src/services/dto/create-service.dto.ts
+++ src/services/dto/create-service.dto.ts
@@ -1,9 +1,41 @@
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
+import {
+  IsBoolean,
+  IsInt,
+  IsNumber,
+  IsOptional,
+  IsString,
+  IsUUID,
+} from 'class-validator';
+
 export class CreateServiceDto {
+  @ApiProperty()
+  @IsUUID()
   tenantId: string;
+
+  @ApiProperty()
+  @IsString()
   name: string;
+
+  @ApiPropertyOptional()
+  @IsOptional()
+  @IsString()
   description?: string;
+
+  @ApiProperty()
+  @IsNumber()
   price: number;
+
+  @ApiProperty()
+  @IsString()
   timezone: string;
+
+  @ApiProperty()
+  @IsInt()
   durationMinutes: number;
+
+  @ApiPropertyOptional()
+  @IsOptional()
+  @IsBoolean()
   isActive?: boolean;
 }
