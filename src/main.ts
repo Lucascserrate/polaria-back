32685420f3b7866ec6c2015e41d2796da6ab9163
Isaias Bermudez <166860546+cheems-chems@users.noreply.github.com>
2026-03-29 17:01:48 -0500

    cors cookie setup

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,19 @@
-import { NestFactory } from '@nestjs/core';
+import cookieParser from 'cookie-parser';
+import { NestApplication, NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
-
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
+import 'dotenv/config';
+const { CLIENT_BASE_URL, ADMIN_CLIENT_BASE_URL } = process.env;
+
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app: NestApplication = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: [CLIENT_BASE_URL, ADMIN_CLIENT_BASE_URL],
+    credentials: true,
+  });
+  app.use(cookieParser());
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
@@ -28,4 +36,4 @@ async function bootstrap() {
   await app.listen(3001);
 }
 
-bootstrap();
+void bootstrap();
